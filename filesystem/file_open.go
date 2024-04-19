9d279936e15877ffe9cb94cf94d7f28c872f6bf6
Humblet,Julian J.M.M <julian.humblet@student.fontys.nl>
2024-04-19 17:54:43 +0200

    Modified error handling

--- filesystem/file_open.go
+++ filesystem/file_open.go
@@ -1,11 +1,14 @@
 package filesystem
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func OpenFile(pathFile string) (*os.File, error) {
 	file, err := os.Open(pathFile)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error opening file: %s", err)
 	}
 
 	return file, nil
